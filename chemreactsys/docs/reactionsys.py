a2abcaa729d416ef3a11ccfaf88484184e0b1d70
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-09-12 18:24:10 -0400

    add reaction sys

---
+++ chemreactsys/docs/reactionsys.py
@@ -0,0 +1,20 @@
+# import packages/modules
+
+# local
+
+
+class ReactionSys:
+
+    def __init__(self):
+        pass
+
+    def load_input(self, input):
+        '''
+        Load input file
+
+        Parameters
+        ----------
+        input: str
+            path of input file
+        '''
+        pass
