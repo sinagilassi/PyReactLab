cec0164878383eb93b41591e2da9f308ab3208f2
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-05-05 16:13:27 -0400

    test lib

--- PyReactLab/configs/__init__.py
+++ PyReactLab/configs/__init__.py
@@ -1,5 +1,10 @@
-from .setting import __description__, __version__, packageName, \
-    packageShortName
+from .setting import __description__, __version__, __author__, __email__, __license__
 
 
-__all__ = ['__description__', '__version__', 'packageName', 'packageShortName']
+__all__ = [
+    "__description__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+]
---
+++ tests/test1.py
@@ -0,0 +1,6 @@
+# import libs
+import PyReactLab as prl
+from rich import print
+
+# check
+print(prl.__version__)
