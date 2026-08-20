5dea4ad9e9c4d4ae61a97d0255b29f6070754b49
sina.gilassi <sina.gilassi@gmail.com>
2026-08-20 00:54:16 -0400

    Add refs to __all__ for improved module exports

--- PyReactLab/models/__init__.py
+++ PyReactLab/models/__init__.py
@@ -1,6 +1,18 @@
 # pyreactlab-core
 from .reactions import Reaction
 
+# refs
+from .refs import (
+    Temperature,
+    Pressure,
+    OperatingConditions,
+    MoleFraction
+)
+
 __all__ = [
     "Reaction",
+    "Temperature",
+    "Pressure",
+    "OperatingConditions",
+    "MoleFraction"
 ]
