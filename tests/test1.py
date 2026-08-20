145dd27c02acd175f9d23495aba0b65a52fe6a31
sina.gilassi <sina.gilassi@gmail.com>
2026-08-20 00:53:46 -0400

    Refactor test1.py to use lowercase package names and improve reaction definitions

--- tests/test1.py
+++ tests/test1.py
@@ -1,11 +1,12 @@
 # import libs
-import PyReactLab as prl
-from PyReactLab import ReactionCore
+import pyreactlab as prl
+from pyreactlab import ReactionCore
 from rich import print
 import pyThermoDB as ptdb
 import pyThermoLinkDB as ptdblink
 from pyThermoLinkDB.models import ModelSource
-from PyReactLab.models import Reaction
+from pyreactlab.models import Reaction
+from pythermodb_settings.models import Temperature, Pressure
 import os
 # locals
 from tests.msource import (
@@ -25,24 +26,25 @@ print(ptdblink.__version__)
 # SECTION: REACTION SYSTEM
 # =======================================
 # NOTE: reaction list format
-reaction_1 = 'CO2(g) + 3H2(g) <=> CH3OH(g) + H2O(g)'
-reaction_2 = 'CO2(g) + H2(g) <=> CO(g) + H2O(g)'
-reaction_3 = 'CO(g) + 2H2(g) <=> CH3OH(g)'
+rxn_1 = Reaction(
+    name='Methanol Formation by CO2-Hydrogenation',
+    reaction='CO2(g) + 3H2(g) <=> CH3OH(g) + H2O(g)',
+    components=[CO2, H2, CH3OH, H2O],
+)
 
-reactions = [
-    {
-        'name': 'Methanol Formation by CO2-Hydrogenation',
-        'reaction': reaction_1
-    },
-    {
-        'name': 'Reverse-Water-Gas-Shift',
-        'reaction': reaction_2
-    },
-    {
-        'name': 'Methanol Formation by CO-Hydrogenation',
-        'reaction': reaction_3
-    }
-]
+rxn_2 = Reaction(
+    name='Reverse-Water-Gas-Shift',
+    reaction='CO2(g) + H2(g) <=> CO(g) + H2O(g)',
+    components=[CO2, H2, CO, H2O],
+)
+
+rxn_3 = Reaction(
+    name='Methanol Formation by CO-Hydrogenation',
+    reaction='CO(g) + 2H2(g) <=> CH3OH(g)',
+    components=[CO, H2, CH3OH],
+)
+
+reactions = [rxn_1, rxn_2, rxn_3]
 
 # =======================================
 # SECTION: load THERMODB
@@ -61,48 +63,73 @@ print(summary)
 reaction_system = prl.create_gas_rxn(
     system_name='Methanol Synthesis',
     reactions=reactions,
-    model_source=model_source
+    model_source=model_source,
+    component_key="Formula-State"
 )
 
+# =======================================
 # SECTION: REACTION SYSTEM PROPERTIES
+# =======================================
 
-# Keq at T
+# NOTE: Keq at T
 res_ = reaction_system.reaction_equilibrium_constant(
     'Methanol Formation by CO2-Hydrogenation',
-    [300.0, "K"],
+    temperature=Temperature(value=300.0, unit="K"),
     message="K_eq at 300 K",
 )
-print(f'K_eq: {res_}')
+print(f'K_eq: ')
+print(res_)
+
 # NOTE: select reaction
 R1: ReactionCore = reaction_system.select_reaction(
-    'Methanol Formation by CO2-Hydrogenation')
-R2: ReactionCore = reaction_system.select_reaction('Reverse-Water-Gas-Shift')
+    'Methanol Formation by CO2-Hydrogenation'
+)
+R2: ReactionCore = reaction_system.select_reaction(
+    'Reverse-Water-Gas-Shift'
+)
 R3: ReactionCore = reaction_system.select_reaction(
-    'Methanol Formation by CO-Hydrogenation')
+    'Methanol Formation by CO-Hydrogenation'
+)
 
 # NOTE: equilibrium constant
 # at std
-print(f'K_eq: {R1.equilibrium_constant_std}')
+print(f'K_eq: ')
+print({R1.equilibrium_constant_std})
+
 # at 300 K
-K_eq_300 = R1.cal_equilibrium_constant([300.0, "K"])
-print(f'K_eq_300: {K_eq_300}')
+# ! van't Hoff method
 K_eq_300 = R1.cal_equilibrium_constant(
-    [300.0, "K"], method="shortcut van't Hoff")
-print(f'K_eq_300: {K_eq_300}')
+    Temperature(value=300.0, unit="K"), method="van't Hoff"
+)
+print(f'K_eq_300: ')
+print(K_eq_300)
+
+# ! shortcut van't Hoff method
+K_eq_300 = R1.cal_equilibrium_constant(
+    Temperature(value=300.0, unit="K"),
+    method="shortcut van't Hoff"
+)
+print(f'K_eq_300: ')
+print(K_eq_300)
 
 # NOTE: energy of reaction
-res_ = R1.cal_reaction_energy([300.0, "K"])
-print(f'En_rxn: {res_}')
+res_ = R1.cal_reaction_energy(
+    Temperature(value=300.0, unit="K")
+)
+print(f'En_rxn: ')
+print(res_)
 
 # NOTE: formatting energies
 res_ = reaction_system.component_formation_energies(
     component_name='CO2-g',
-    temperature=[300.0, "K"],
+    temperature=Temperature(value=300.0, unit="K")
 )
-print(f'formation energies: {res_}')
+print(f'formation energies: ')
+print(res_)
 
+# =======================================
 # SECTION: EQUILIBRIUM CALCULATION
-
+# =======================================
 # NOTE: mole fraction
 mole_fraction = {
     'CO2-g': 1,
