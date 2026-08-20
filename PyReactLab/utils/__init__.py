d66800c1526996a8a99070e5502e94b69fbe5fcb
sina.gilassi <sina.gilassi@gmail.com>
2026-08-20 00:54:01 -0400

    Remove unused refs module and clean up exports in utils

--- PyReactLab/utils/__init__.py
+++ PyReactLab/utils/__init__.py
@@ -1,19 +1,8 @@
 # export
 from .tools import model_source_checker
 from .chemreact import ChemReactUtils
-# refs
-from .refs import (
-    Temperature,
-    Pressure,
-    OperatingConditions,
-    MoleFraction
-)
 
 __all__ = [
     "model_source_checker",
     "ChemReactUtils",
-    "Temperature",
-    "Pressure",
-    "OperatingConditions",
-    "MoleFraction"
 ]
